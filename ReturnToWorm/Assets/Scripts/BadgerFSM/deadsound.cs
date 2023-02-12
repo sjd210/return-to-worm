7e6c3ab236f317258aa0e6395457a553584b1504
Will <78590787+SmokeyCrab@users.noreply.github.com>
2023-02-12 14:02:07 +0000

    All animals working

--- ReturnToWorm/Assets/Scripts/BadgerFSM/deadsound.cs
+++ ReturnToWorm/Assets/Scripts/BadgerFSM/deadsound.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class deadsound : MonoBehaviour
 {
     public AudioSource aud;
+    public AudioClip dead_sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
     void play_sound(){
-        aud.Play();
+
+        aud = GetComponent<AudioSource>();
+        aud.PlayOneShot(dead_sound, 0.7F);
     }
 }
